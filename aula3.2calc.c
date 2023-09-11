f28c52680029eafa8d17178de7b81c54f5e2d50b
MATEUS AMORIM DE SOUSA E SILVA <mateusamorim.silva@a.ucb.br>
2023-09-11 10:17:20 -0300

    first commit

---
+++ aula3.2calc.c
@@ -0,0 +1,71 @@
+
+#include <stdio.h>
+
+int main(){
+    float n1, n2, r;
+    char op;
+
+    printf("digite a operacao:\n");
+    scanf("%c", &op);
+
+
+
+
+
+while (op!='b')
+{
+
+    printf("digite o numero1:\n");
+    scanf("%f", &n1);
+
+    printf("digite o numero 2:\n");
+    scanf("%f", &n2);
+
+    
+    //adição
+
+    if (op=='a')
+    {
+        r = n1 + n2;
+        printf("resultado: %2.f\n", r);
+    }
+
+    //subtração
+
+    else if (op=='s')
+    {
+        r = n1 - n2;
+        printf("resultado: %2.f\n", r);
+    } 
+
+    //multiplicação
+    
+    else if (op=='m')
+    {
+        r = n1 * n2;
+        printf("resultado: %2.f\n", r);
+    }
+
+    //divisão
+
+    else if (op=='d')
+    {
+        r = n1/n2; 
+        printf("resultado: %2.f\n", r);
+    }
+
+
+    //nenhum
+
+    else{
+        printf("caracter nao reconhecido, digite apenas a, s, m, d, p\n");
+        scanf("%c", &op);
+        
+    }
+    
+   
+
+    
+}
+
+}
